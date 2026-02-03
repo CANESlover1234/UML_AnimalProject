9cba87efeb50ba1b3daa92081625f6937ec8a3b7
CANESlover1234 <jdlagunas@chicagotechacademy.org>
2026-02-03 11:20:58 -0600

    Uploading all files

---
+++ Animal.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+public:
+
+	//name()
+	string name;
+	//age()
+	int age;
+	//walk()
+	
+	//eat
+	//Print Info
+	void printInfo()
+	{
+		cout << "You Created " << name << endl;
+	}
+};
+
